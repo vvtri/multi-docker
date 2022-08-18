40d12c4c3d5edde1d134937ddbca509df988e0b1
vvtri <82482496+vvtri@users.noreply.github.com>
2022-08-18 14:37:13 +0700

    initial commit

--- server/index.js
+++ server/index.js
@@ -19,6 +19,8 @@ const pgClient = new Pool({
   port: keys.pgPort,
 });
 
+console.log('some thing');
+
 pgClient.on("connect", (client) => {
   client
     .query("CREATE TABLE IF NOT EXISTS values (number INT)")
